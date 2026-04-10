efeb41c14a050f2e40896d9211e0163ead8f848c
ai-lman <tullaris2000@gmail.com>
2026-04-10 13:19:57 +0200

    Update create_image_sequence.py

--- create_image_sequence.py
+++ create_image_sequence.py
@@ -1,6 +1,4 @@
 """
-This program was designed to run with python 3 in a Spyder environnement, with usual packages plus npTDMS,
-cv2 and local libraries found in the folder diagnostics and utils of the environment. 
 Only the imput parameter section should be modified.
 
 They are: - [shot] int
@@ -21,6 +19,8 @@ They are: - [shot] int
               the name of the folder in which all the code architecture is 
           - [fps] float
               the frame rate of the saved movie 
+          - [start] int
+              the time in µs at which the movie will begin
           - [length] int
               the time lenght in µs of the movie/image sequence
           - [sus_probes] numpy array
@@ -28,16 +28,16 @@ They are: - [shot] int
           - [k] int
               the extent of the colorbar in terms of standard variation of the signal
 
-Take the data from the DDAQ file or computed by the extract_all_data program to plot images of NORTH and then 
-generate a video. The obtained plots are stored in a sub folder the Figure folder created if not existing.
-Can be raw or fluctuating data.
+Take the data from the DDAQ file or computed by the simulation to plot images of a poloidal cross-section
+of NORTH and then generate a video. The obtained plots are stored in a sub folder the Figure folder created 
+if not existing. Can be raw or fluctuating data.
 """
 
 #Find the path to local libraries (modify the PYTHON PATH)
 import sys
-sys.path.append('E:\\north_diagnostics\\diagnostics')
-sys.path.append('E:\\north_diagnostics\\utils')
-sys.path.append('E:\\north_diagnostics')
+sys.path.append('C:\\Users\\tulla\\Perso\\north_diagnostics\\diagnostics')
+sys.path.append('C:\\Users\\tulla\\Perso\\north_diagnostics\\utils')
+sys.path.append('C:\\Users\\tulla\\Perso\\north_diagnostics')
 
 #Import all useful libraries
 import numpy as np
@@ -45,16 +45,21 @@ import matplotlib.pyplot as plt
 import os
 import utils.dau
 
-#Input parameters
-shot = 10164
+
+
+"""
+Input parameters
+"""
+shot = 10915
 bias_type = 'ion_saturation_current'
-data_type = 'fluctuations'
+data_type = 'fluctuations' #'fluctuations'
 data_origin = 'experiment'
 current_value = 0
-path = 'E:/north_diagnostics'
+path = 'C:/Users/tulla/Perso/north_diagnostics'
 fps = 15
+start = 800000
 length = 1000
-sus_probes = [2]
+sus_probes = [22]
 k = 2
 
 
@@ -65,13 +70,12 @@ Main program: from now, all shall remain untouched.
 """
 #Extract and cleaning data from .txt file
 #Activated probes for that type of data
-studied_probes = np.array([1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,
-                           26,27,28,29,30,31,32,33,38,39,40,41,42,43,44,45,46,47,48,49])
+studied_probes = np.array([1,2,3,5,6,7,8,9,10,11,13,14,15,17,18,19,20,21,23,24,25,
+                           26,27,28,29,30,31,32,33,38,39,41,42,43,44,45,46,47,48,49])
 
-data = np.genfromtxt(f"{path}/Data/probe_data{shot}.txt", delimiter=';', skip_header=1)
+#data = np.genfromtxt(f"{path}/Data/probe_data{shot}.txt", delimiter=';', skip_header=1)
 if data_origin == 'experiment':
-      start = 500000
-      all_data = utils.dau.read_probe_data(shot, path, 'ion_saturation_current', 1/75, 
+      all_data = utils.dau.read_probe_data(shot, path, bias_type, 1/75, 
                                          studied_probes-1, 0, 1, False)
       data = all_data[start:start+length, :]
       print(f"Data shot {shot} loaded with success")
@@ -84,15 +88,16 @@ if data_origin == 'experiment':
 elif data_origin == 'simulation': 
       #Activated probes for that type of data
       studied_probes = np.array([1,2,3,4,5,6,7,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,
-                                 26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50])
+                                 26,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50])
       
-      all_data = np.genfromtxt(f"{path}/Data/probeIsat_Bvertsweep.txt", skip_header=5)
+      all_data = np.genfromtxt(f"{path}/Data/probeIsat_fullmodel.txt", skip_header=5)
       print("Data probe loaded with success")
       
       #Take only the data for the given current value
-      n_sweep = int(current_value//2.5)
-      data = all_data[n_sweep*1001+100:(n_sweep+1)*1001-100, 1:]
-      data[:, 0] = data[:, 0]*1E-3
+      start = 2000
+      n_sweep = int((current_value)//20)
+      data = - all_data[n_sweep*5001+start:(n_sweep+1)*5001-2000, :]
+      data[:, 0] = - data[:, 0]*1E-3
       
       #Create a folder to store data if it doesn't exist
       if not os.path.exists(f"{path}/Figures/{int(current_value)}_{bias_type}_{data_type}/"):
@@ -103,8 +108,10 @@ else:
     print('WARNING: the data origin is not recognized')
     sys.exit()
     
-
+#Add a boundary condition for the interpolation method
 bc = 2.26E-5 #in A
+
+#If activated, compute the fluctuations of the data and adapt boundary condition
 if data_type=='fluctuations':
   data[:,1:] = data[:,1:] - np.mean(data[:,1:], axis=0, keepdims=True)
   bc = 0
@@ -130,8 +137,12 @@ for i in range(len(data[:,0])):
     output = utils.dau.plot_2D_data(data[i, :], shot, path, bias_type, data_type, i, 
                                     activated_probes, vmin, vmax, fig, bc)
     plt.legend()
+    
+    #Necessary to name images with a fixed format to avoid confusion when the video is read
     std_format = "{:0"+str(int(np.log(len(data[:,0])+1)/np.log(10))+1)+"d}"
     std_format = std_format.format(i)
+    
+    #Adapt the layout
     if data_origin=='experiment':
         plt.suptitle(f"Shot {shot} {data_type} {bias_type} at time {data[i,0]:.6} s")
         plt.savefig(f"{path}/Figures/{shot}_{bias_type}_{data_type}/{std_format}.jpg")
@@ -140,9 +151,11 @@ for i in range(len(data[:,0])):
         plt.savefig(f"{path}/Figures/{int(current_value)}_{bias_type}_{data_type}/{std_format}.jpg")
     plt.clf()
     
+    #Display a confirmation message that the process happened
     print(output)
 plt.close()
 
-#Save those images in a movie (.avi or .tiff) => to be found in the figure folder
-output = utils.dau.video_2D(data, data_origin, shot, current_value, path, bias_type, data_type, fps)
+#Save those images in a movie (.avi format) => to be found in the figure folder
+output = utils.dau.video_2D(data, data_origin, shot, current_value, path, bias_type, data_type, fps, 
+                            int(start/1E3))
 print(output)
